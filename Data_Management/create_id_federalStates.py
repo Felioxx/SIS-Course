031af364f0bf90c7154054801aecbce8a1eaac25
Eva :) <evalangstein@freenet.de>
2024-10-23 17:43:42 +0200

    Add functionality to simplify polygons

--- Data_Management/create_id_federalStates.py
+++ Data_Management/create_id_federalStates.py
@@ -3,19 +3,23 @@ import csv
 import pandas as pd
 
 
-df = pd.read_csv("Data_Management\\federalStates.csv")
-d_names = df.Name #you can also use df['column_name']
-d_geometry = df.geometry
+gdf = gpd.read_file("Graph\Shapes\\federalStates.shp")
+# Simplify the geometries
+gdf['geometry'] = gdf['geometry'].simplify(tolerance=0.01, preserve_topology=True)
+d_names = gdf['Name']
+d_geometry = gdf['geometry']
 
 i = 1
 D_ID = []
-for d in d_names:
+for c in d_names:
     D_ID.append("D"+str(i))
     i = i+1
 
-districts = {"D_ID":D_ID, "Name": d_names, "Geometry": d_geometry}
+districts = {"D_ID": D_ID, "Name": d_names, "Geometry": d_geometry}
+df_districts = pd.DataFrame(districts)
 
-df_districts = pd.DataFrame(districts) 
-df_districts.to_csv('Data_Management\id_federalStates.csv', index=False, sep = ";")
+# Konvertieren der Geometrie zu WKT (Well-Known Text) für den Export in CSV
+df_districts['Geometry'] = df_districts['Geometry'].apply(lambda x: x.wkt)
 
-# print(c_names)
+# Speichern als CSV
+df_districts.to_csv('Data_Management/id_federalStates.csv', index=False, sep=",")
