14cb4cea319273b4c834129babb45cb6516c7bce
Eva :) <evalangstein@freenet.de>
2024-10-30 15:59:19 +0100

    centroid

--- Graph/Data_Management/cities_centroid.py
+++ Graph/Data_Management/cities_centroid.py
@@ -7,10 +7,17 @@ from shapely.geometry import LineString, MultiPoint, Polygon
 df = pd.read_csv("Graph\id_cities.csv")
 c_geometry = df.Geometry
 c_centroid = []
+print(c_geometry[0].replace("POLYGON", "").replace("(","").replace(")","").split(",")[0].split(" "))
 
-
+coord_pairs = []
 for city in c_geometry:
-    print(city)
-    c_centroid.append(Polygon.centroid(city))
+    coord_pairs.append(city.replace("POLYGON ", "").replace("MULTI", "").replace("(","").replace(")","").split(","))
 
-print(c_centroid)
+for pair in coord_pairs:
+    c = pair[0].split(" ")
+    c_centroid.append((float(c[0]), float(c[1])))
+
+polygon = Polygon(c_centroid)
+print(polygon.centroid)
+
+#print(c_centroid)
