f522a56ac24f5060b2cc5b204245078e2bb38ba6
Laxeros07 <annestaskie@gmail.com>
2024-11-11 13:53:49 +0100

    cities population

--- Graph/Data_Management/cities_centroid.py
+++ Graph/Data_Management/cities_centroid.py
@@ -2,15 +2,15 @@ import geopandas as gpd
 import csv
 import pandas as pd
 from shapely.geometry import LineString, MultiPoint, Polygon
+import shapely.wkt
 
 
 df = pd.read_csv("Graph\id_cities.csv")
 c_geometry = df.Geometry
 c_centroid = []
 
-
 for city in c_geometry:
-    print(city)
-    c_centroid.append(Polygon.centroid(city))
+    p = shapely.wkt.loads(city)
+    c_centroid.append(p.centroid())
 
 print(c_centroid)
