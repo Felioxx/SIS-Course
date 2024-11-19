be145436cd08552ecc8ed4f7f0d9a4baf4a7f60c
Laxeros07 <annestaskie@gmail.com>
2024-11-19 15:38:09 +0100

    app frontend

---
+++ App/public/javascripts/leaflet.js
@@ -0,0 +1,14 @@
+// Map with NRW bounds
+var map = L.map("map").setView([51.51588878700843, 7.475327389339492], 7);
+
+mapLink = '<a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>';
+var osm = L.tileLayer("http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+  attribution: "&copy; " + mapLink + " Contributors",
+  maxZoom: 18,
+}).addTo(map);
+
+var baseMaps = {
+  OpenStreetMap: osm,
+};
+
+var layerControl = L.control.layers(baseMaps).addTo(map);
