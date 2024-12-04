68d71323ffa7ec148816505f8fa2c4971a525433
Laxeros07 <annestaskie@gmail.com>
2024-12-04 16:50:15 +0100

    running horse

--- App/public/python/neo4j_QA.py
+++ App/public/python/neo4j_QA.py
@@ -9,6 +9,30 @@ import asyncio
 import json
 import re
 
+from langchain.prompts.prompt import PromptTemplate
+
+
+CYPHER_GENERATION_TEMPLATE = """
+Task:Generate Cypher statement to query a graph database.
+Instructions:
+Use only the provided relationship types and properties in the schema.
+Do not use any other relationship types or properties that are not provided.
+Schema:
+{schema}
+Cypher examples:
+# Which cities lie left of Münster?
+MATCH p=(C:City WHERE C.Name = "Münster") -[T:touches WHERE T.`Rel_Position`in ["western","southwestern","northwestern"]]->(:City) RETURN p
+
+Note: Do not include any explanations or apologies in your responses.
+Do not respond to any questions that might ask anything else than for you to construct a Cypher statement.
+Do not include any text except the generated Cypher statement.
+
+The question is:
+{question}"""
+CYPHER_GENERATION_PROMPT = PromptTemplate(
+    input_variables=["schema", "question"], template=CYPHER_GENERATION_TEMPLATE
+)
+
 url = "neo4j+ssc://f02e0524.databases.neo4j.io:7687"
 username = "neo4j"
 password = "w60PF-SK2gGIlDII6zZMw8XMo67mqIFSrPU54_E3AU4"
@@ -26,7 +50,8 @@ chain = GraphCypherQAChain.from_llm(
     cypher_llm=ChatOpenAI(temperature=0, model="gpt-4o-mini"), # gpt-4o-mini	gpt-3.5-turbo
     qa_llm=ChatOpenAI(temperature=0, model="gpt-3.5-turbo-16k"),
     verbose=False,
-    allow_dangerous_requests=True
+    allow_dangerous_requests=True,
+    cypher_prompt=CYPHER_GENERATION_PROMPT
 )
 
 # Async function in Python - waits for the neo4j request before printing the answer
