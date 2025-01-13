49f65a2521f3b98e28dec0f167c5bdec42fcbdc2
Laxeros07 <annestaskie@gmail.com>
2025-01-13 19:39:03 +0100

    layer order on map

--- App/public/javascripts/chat.js
+++ App/public/javascripts/chat.js
@@ -250,7 +250,7 @@ async function parseCSV(searchIDs) {
 
   const rows = parsed.data;
   // get the geometries and add them to the leaflet map
-  for (const item of searchIDs) {
+  for (const item of searchIDs.slice().reverse()) {
     let result = rows.find((row) => row.ID == item.id);
 
     // get the type of the geometry
@@ -273,7 +273,6 @@ async function parseCSV(searchIDs) {
             layer.bindPopup(item.name);
           },
         });
-
         fsGeoJsonLayerGroup.addLayer(newLayer);
         break;
       case "A":
@@ -323,6 +322,7 @@ async function parseCSV(searchIDs) {
           },
           onEachFeature: function (feature, layer) {
             layer.bindPopup(item.name);
+            layer.bringToFront();
           },
         });
         cityGeoJsonLayerGroup.addLayer(newLayer);
@@ -331,10 +331,10 @@ async function parseCSV(searchIDs) {
     //map.flyToBounds(geoJsonLayerGroup.getBounds());
   }
   // make Layer Control
-  layerControl.addOverlay(fsGeoJsonLayerGroup, "Federal State");
-  layerControl.addOverlay(adGeoJsonLayerGroup, "Administrative Districts");
-  layerControl.addOverlay(districtGeoJsonLayerGroup, "Districts");
   layerControl.addOverlay(cityGeoJsonLayerGroup, "Cities");
+  layerControl.addOverlay(districtGeoJsonLayerGroup, "Districts");
+  layerControl.addOverlay(adGeoJsonLayerGroup, "Administrative Districts");
+  layerControl.addOverlay(fsGeoJsonLayerGroup, "Federal State");
 
   var group = L.featureGroup([
     cityGeoJsonLayerGroup,
